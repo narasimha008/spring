510e06c329d362d5e64fd0e399a804e3d9e0d137
Narasimha Mandla <shalivahana19@gmail.com>
2019-09-24 12:20:58 +0530

    few spring modules

---
+++ web/springboot-thymeleaf-demo/src/main/java/com/sivalabs/demo/SpringBootThymeleafApplication.java
@@ -0,0 +1,15 @@
+package com.sivalabs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Siva on 18-02-2016.
+ */
+@SpringBootApplication
+public class SpringBootThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootThymeleafApplication.class, args);
+    }
+}
